f9462f942e3dc850021c19d52025e14dd9405570
Wang ZhiDong <6027021846@iwhalecloud.com>
2019-09-24 19:20:27 +0800

    test

--- src/main/java/my/hehe/demo/services/vo/ClassDeployVO.java
+++ src/main/java/my/hehe/demo/services/vo/ClassDeployVO.java
@@ -1,18 +1,32 @@
 package my.hehe.demo.services.vo;
 
-import io.vertx.core.json.JsonObject;
 import my.hehe.demo.common.StreamUtils;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.Calendar;
-import java.util.Map;
+import java.util.Date;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipInputStream;
-import java.util.zip.ZipOutputStream;
 
 public class ClassDeployVO extends DeployVO {
+
+  private String backUpDir;
+  private SimpleDateFormat simpleDateFormat= new SimpleDateFormat("yyyy-MM-dd-HH-mm-ss");
+
+  @Override
+  public void deployAllAfter(ZipInputStream zipInputStream) throws Throwable {
+    this.backUpDir =null;
+    super.deployAllAfter(zipInputStream);
+  }
+
+  @Override
+  public void deployAllBefore(ZipInputStream zipInputStream) throws Throwable {
+    this.backUpDir = null;
+    this.backUpDir =(".backUp"+File.separator+simpleDateFormat.format(new Date())).replace("\\", "/");;
+    super.deployAllBefore(zipInputStream);
+  }
 
   @Override
   public void deploySingle(ZipInputStream zipInputStream, ZipEntry zipEntry) throws Throwable {
@@ -24,9 +38,15 @@ public class ClassDeployVO extends DeployVO {
       System.out.println(zipName + "-->" + deployName);
       if (file.exists()) {
         String bakName = null;
-        bakName = deployName + "." + ((Calendar.getInstance().getTimeInMillis()) + ".bak");
-        bakName = bakName.replace("\\", "/");
-        System.out.println(String.format("%s exists ! backup to %s %s", deployName, bakName, file.renameTo(new File(bakName)) ? "success" : "fail"));
+        /*bakName = deployName + "." + ((Calendar.getInstance().getTimeInMillis()) + ".bak");
+        bakName = bakName.replace("\\", "/");*/
+        bakName = new StringBuilder(deployName).insert(this.getPath().length(),File.separator+backUpDir).toString().replace("\\", "/");
+        File bakFile=new File(bakName);
+        File bakPraFile=bakFile.getParentFile();
+        if(!bakPraFile.exists()){
+          bakPraFile.mkdirs();
+        }
+        System.out.println(String.format("%s exists ! backup to %s %s", deployName, bakName, file.renameTo(bakFile) ? "success" : "fail"));
 
       }
       File parentFile = file.getParentFile();
