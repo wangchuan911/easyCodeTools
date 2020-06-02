2e19fd577042fe6a313b7d9d6a1606f0f66b8f23
Wang ZhiDong <6027021846@iwhalecloud.com>
2020-06-02 10:59:05 +0800

    test

--- src/main/java/my/hehe/demo/common/PromiseFlow.java
+++ src/main/java/my/hehe/demo/common/PromiseFlow.java
@@ -9,49 +9,56 @@ import java.util.*;
 public class PromiseFlow {
 
 	static Vertx vertx = Vertx.vertx();
-	final PromiseFlow.FlowUnit head;
+	final PromiseFlow.FlowUnit head = new PromiseFlow.FlowUnit(this.size = 0, "start", null);
 	int size;
-	STATE state;
-	Handler<PromiseFlow> finalHandler;
+	STATE state = STATE.PREPARE;
+	Handler<FlowEndUnitState> finalHandler;
 	Handler<Throwable> throwHandler;
 
-	public PromiseFlow(Handler<FlowUnit> handle) {
-		state = STATE.PREPARE;
-		final PromiseFlow.FlowUnit flowUnit = new PromiseFlow.FlowUnit(this.size = 1, "start", handle);
-		this.head = flowUnit;
+	public PromiseFlow(Handler<FlowUnitState> handle) {
+		this.head.handler = handle;
+		this.size = ++this.head.index;
 	}
 
-	void execute(PromiseFlow.FlowUnit flowUnit) {
+	public PromiseFlow() {
+	}
+
+	void execute(PromiseFlow.FlowUnit flowUnit, Map<String, Object> params) {
 		this.isRuning(true);
 		if (flowUnit == null) {
-			this.end(STATE.FINISH);
+			this.end(STATE.FINISH, params);
 			return;
 		}
 		vertx.executeBlocking(promise -> {
-			flowUnit.promise = promise;
-			flowUnit.handle();
+			flowUnit.handle(new FlowUnitState(promise, params));
 		}, asyncResult -> {
 			if (asyncResult.succeeded()) {
-				this.execute(flowUnit.nextUnit);
+				this.execute(flowUnit.nextUnit, params);
 			} else {
 				if (throwHandler != null)
 					throwHandler.handle(asyncResult.cause());
-				this.end(STATE.FAIL);
+				this.end(STATE.FAIL, params);
 			}
 		});
 	}
 
-	public PromiseFlow then(Handler<PromiseFlow.FlowUnit> handle) {
+	public PromiseFlow then(Handler<FlowUnitState> handle) {
 		return this.then(null, handle);
 	}
 
-	public PromiseFlow then(String name, Handler<PromiseFlow.FlowUnit> handle) {
+	public PromiseFlow then(String name, Handler<FlowUnitState> handle) {
 		this.isPrepare(true);
 		PromiseFlow.FlowUnit idxUnit = this.head;
 		for (int i = 0; i < this.size - 1; i++) {
 			idxUnit = idxUnit.nextUnit;
 		}
-		idxUnit.nextUnit = new PromiseFlow.FlowUnit(++this.size, name, handle);
+		if (this.size == 0) {
+			idxUnit.handler = handle;
+			idxUnit.name = name;
+			idxUnit.index = ++this.size;
+		} else {
+			idxUnit.nextUnit = new PromiseFlow.FlowUnit(++this.size, name, handle);
+		}
 		return this;
 	}
 
@@ -61,47 +68,56 @@ public class PromiseFlow {
 		return this;
 	}
 
-	public PromiseFlow finalThen(Handler<PromiseFlow> finalHandler) {
+	public PromiseFlow finalThen(Handler<FlowEndUnitState> finalHandler) {
 		this.isPrepare(true);
 		this.finalHandler = finalHandler;
 		return this;
 	}
 
-	void end(STATE state) {
+	void end(STATE state, Map<String, Object> params) {
 		this.isRuning(true);
 		this.state = state;
 		if (this.finalHandler != null) {
-			this.finalHandler.handle(this);
+			this.finalHandler.handle(new FlowEndUnitState(params));
 		}
 //		vertx.close();
 	}
 
 	public void start() {
-		this.isPrepare(true);
-		state = STATE.RUNING;
-		execute(this.head);
+		this.start(null);
 	}
 
-	class FlowUnit {
-		private int index;
-		private String name;
-		private Handler<PromiseFlow.FlowUnit> handler;
-		private Promise promise;
-		private Map<String, Object> param;
-		private PromiseFlow.FlowUnit nextUnit;
+	public void start(Map<String, Object> params) {
+		this.isPrepare(true);
+		state = STATE.RUNING;
+		execute(this.head, params == null ? new HashMap<>() : params);
+	}
 
-		FlowUnit(int index, String name, Handler<PromiseFlow.FlowUnit> handle) {
-			this.name = name;
-			this.handler = handle;
-			this.index = index;
+	public final class FlowEndUnitState extends FlowUnitState {
+		FlowEndUnitState(Map<String, Object> param) {
+			super(null, param);
 		}
 
-		public String getName() {
-			return name;
+		public void next() {
+			throw new RuntimeException("is complete");
 		}
 
-		void handle() {
-			this.handler.handle(this);
+		public void fail(String s) {
+			throw new RuntimeException("is complete");
+		}
+
+		public void fail(Throwable t) {
+			throw new RuntimeException("is complete");
+		}
+	}
+
+	public class FlowUnitState {
+		Promise promise;
+		Map<String, Object> param;
+
+		FlowUnitState(Promise promise, Map<String, Object> param) {
+			this.promise = promise;
+			this.param = param;
 		}
 
 		public void next() {
@@ -127,6 +143,29 @@ public class PromiseFlow {
 		}
 	}
 
+	class FlowUnit {
+		int index;
+		String name;
+		Handler<FlowUnitState> handler;
+		PromiseFlow.FlowUnit nextUnit;
+
+		FlowUnit(int index, String name, Handler<FlowUnitState> handle) {
+			this.name = name;
+			this.handler = handle;
+			this.index = index;
+		}
+
+		public String getName() {
+			return name;
+		}
+
+		void handle(FlowUnitState flowUnitState) {
+			this.handler.handle(flowUnitState);
+		}
+
+
+	}
+
 	public static void main(String[] args) {
 		new PromiseFlow(flowUnit -> {
 			System.out.println(flowUnit.toString());
@@ -147,7 +186,7 @@ public class PromiseFlow {
 			System.out.println("fin");
 		}).start();
 
-		new PromiseFlow(flowUnit -> {
+		new PromiseFlow().then(flowUnit -> {
 			System.out.println(flowUnit.toString());
 			System.out.println(1);
 			flowUnit.next();
@@ -198,5 +237,13 @@ public class PromiseFlow {
 			return false;
 		}
 		return true;
+	}
+
+	public boolean isComplete() {
+		return this.state == STATE.FINISH;
+	}
+
+	public boolean isError() {
+		return this.state == STATE.FAIL;
 	}
 }
