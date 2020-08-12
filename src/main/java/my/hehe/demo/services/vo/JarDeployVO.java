9c69d9c285158eb73cc449405bd397e106e456a3
Wang ZhiDong <6027021846@iwhalecloud.com>
2020-08-12 16:40:47 +0800

    test

--- src/main/java/my/hehe/demo/services/vo/JarDeployVO.java
+++ src/main/java/my/hehe/demo/services/vo/JarDeployVO.java
@@ -61,6 +61,7 @@ public class JarDeployVO extends ClassDeployVO {
 	public void deployAllAfter(ZipInputStream zipInputStream) throws Throwable {
 
 		try {
+			if (fileList.size() == 0) return;
 			System.out.println();
 			String command = String.format(CMD, this.getPath(), System.getProperty("jarBinPath"), newFile.getName(), fileList.stream().collect(Collectors.joining(isWindows ? "' '" : " ")));
 			System.out.println(command);
